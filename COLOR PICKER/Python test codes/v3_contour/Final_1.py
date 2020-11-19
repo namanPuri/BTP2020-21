1322087ebcdbd4bae309e9091d77c023e749d4b4
namanPuri <namanpuri1712@gmail.com>
2020-11-19 15:45:54 +0530

    v3 take 2

--- COLOR PICKER/Python test codes/v3_contour/Final_1.py
+++ COLOR PICKER/Python test codes/v3_contour/Final_1.py
@@ -6,26 +6,27 @@ import time
 from pygame import mixer
 
 btn_pin = 15
-mode = True # 1 for color and 2 for ph
+mode = 0 # 1 for color and 2 for ph
 st_time = -1
 index=["color_name","R","G","B"]
+index1=["color_name","hash","R","G","B"]
 frameWidth = 640
 frameHeight = 480
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 prev_result_mode2 = "nill"
-
+count = 0
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(btn_pin, GPIO.IN)
 mixer.init()
 
 def getColorName(R,G,B):
-	if(mode):
-		csv = pd.read_csv('colorsT10.csv', names=index, header=None,encoding='latin-1')
+    if(mode):
+        csv = pd.read_csv('colorsV3.csv', names=index1, header=None,encoding='latin-1')
     else:
-    	csv = pd.read_csv('colorsV3.csv', names=index, header=None,encoding='latin-1')
+        csv = pd.read_csv('colorsT10.csv', names=index, header=None,encoding='latin-1')
     minimum = 10000
     for i in range(len(csv)):
         d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
@@ -37,12 +38,15 @@ def getColorName(R,G,B):
 
 def getContours(img,imgContour):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    red_count=0 , blue_count=0 ,other=0 , prev_count=0
+    red_count=0
+    blue_count=0
+    other=0
+    prev_area=0
     for cnt in contours:
         area = cv2.contourArea(cnt)
         areaMin = 2000
-        areaMax = 15000
-        if (area > areaMin and area < areMax and area > prev_area):
+        areaMax = 20000
+        if (area > areaMin and area < areaMax and area > prev_area):
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
@@ -51,38 +55,46 @@ def getContours(img,imgContour):
                 break
 
             print(len(approx))
-			for i in range 9:
-		        x , y , w, h = cv2.boundingRect(approx)
-		        result = detect_color(x+10,y+10,x+w-20,y+h-20)
-		        if(result == 0):
-		        	red_count +=1
-		        elif(result == 1):
-		        	blue_count +=1
-		        else:
-		        	other +=1
+            x , y , w, h = cv2.boundingRect(approx)
+            result = detect_color(x+10,y+10,x+w-20,y+h-20)
+            if(result == 0):
+                red_count +=1
+            elif(result == 1):
+                blue_count +=1
+            else:
+                other +=1
 
-		prev_area = cv2.contourArea(cnt)
-		if(red_count > 3):
-			if(prev_result_mode2 == "red"):
-				#speak red again
-			else:
-				if(prev_result_mode2 != "nill"):
-					#speak red
-				else:
-					#speak its red now
-			prev_result_mode2 = "red"
-		if(blue_count > 3):
-			if(prev_result_mode2 == "blue"):
-				#speak blue again
-			else:
-				if(prev_result_mode2 != "nill"):
-					#speak blue
-				else:
-					#speak its blue now
-			prev_result_mode2 = "blue"
-		else:
-			#speak no change
-			prev_result_mode2 = "nill"
+        prev_area = cv2.contourArea(cnt)
+    print(red_count," ",blue_count,"  ",other,"  final output   ")
+    if(red_count > 3):
+        if(prev_result_mode2 == "red"):
+            print("Red again")
+            #speak red again
+        else:
+            if(prev_result_mode2 != "nill"):
+                print("Red")
+                #speak red
+            else:
+                print("Its Red now")
+                #speak its red now
+        prev_result_mode2 = "red"
+    if(blue_count > 3):
+        if(prev_result_mode2 == "blue"):
+            print("Blue again")
+            #speak blue again
+        else:
+            if(prev_result_mode2 != "nill"):
+                print("Blue")    
+                #speak blue
+            else:
+                print("Its Blue now")
+                #speak its blue now
+        prev_result_mode2 = "blue"
+    else:
+        print("No ph paper.....")
+        #speak no change
+        prev_result_mode2 = "nill"
+    
 
 
 
@@ -121,7 +133,7 @@ def detect_color(s1,s2,e1,e2):
             else:
                 white +=1
 
-    print(red,"   ",blue,"    ",white,"     ",ph,"    ",count)
+    print(red,"   ",blue,"    ",white,"     ",ph,"    ",count," step output ")
 
 
     if(red > blue and red > 10):
@@ -170,55 +182,51 @@ def func_mode1():
 
 
 while True:
-    count = 0
     success, img = cap.read()
-	time.sleep(0.1)
+    time.sleep(0.1)
     imgContour = img.copy()
-    """
     if (GPIO.input(btn_pin) == False):
         time.sleep(0.01)
         if (GPIO.input(btn_pin) == False):
-        	time.sleep(0.2)
-        	if (GPIO.input(btn_pin) == True):
-        		count+=1
-        		if(st_time==-1):
-        			st_time = time.time()
+            time.sleep(0.3)
+            if (GPIO.input(btn_pin) == True):
+                count+=1
+                if(st_time==-1):
+                    st_time = time.time()
 
-        		count +=1
+    if(st_time!=-1):
+        if(time.time()-st_time>1.2 and time.time()-st_time<1.8):
+            print(count)
+            if(count==1):
+                print(mode)
+                if(mode):
+                    func_mode1()
+                else:
+                    func_mode2()
 
-	if(st_time!=-1):
-		if(time.time()-st_time>1 and time.time()-st_time<1.5):
-			if(count==1):
-				if(mode):
-					func_mode1()
-				else:
-					func_mode2()
-
-			else:
-				mode = not mode
-				if(mode):
-					#sound
-				else:
-					#sound
-
-
-			count=0
-			st_time=-1;
-
-		if(time.time()-st_time>1.5):
-			count=0
-			st_time=-1
-
-			
+            else:
+                mode = not mode
+                if(mode):
+                    print("changed mode 1")
+                    #sound
+                else:
+                    print("changed mode 2")
+                    #sound
 
 
+            count=0
+            st_time=-1;
 
-
-
-
-		
+        if(time.time()-st_time>1.8):
+            count=0
+            st_time=-1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    img = cv2.flip(img,1)
     
-"""
+    cv2.imshow('CAMERA',img)
+
+        
 cap.release()
 
 cv2.destroyAllWindows()
