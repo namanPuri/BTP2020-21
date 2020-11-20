6e61c73855f0de1224b230194cbaa46e279b46d4
Genos001 <Vishal@250>
2020-11-20 17:23:48 +0530

    1 day bef 3

--- COLOR PICKER/Python test codes/v3_contour/Final_1.py
+++ COLOR PICKER/Python test codes/v3_contour/Final_1.py
@@ -106,36 +106,7 @@ def getContours(img,imgContour):
         prev_result_mode2 = "nill"
     
 
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
+
 
 
 
@@ -170,8 +141,6 @@ def detect_color(s1,s2,e1,e2):
         for y in range( s2 , e2-1,12) :
             if frameHeight <= x+15 or frameWidth <= y+15:
                 break 
-
-
             for z in range (0,2):
                 if(z==0):
                     roi = red[x:x+10, y:y+10]
@@ -198,14 +167,6 @@ def detect_color(s1,s2,e1,e2):
                     Ph +=1
                 else:
                     Black +=1
-                    
-                cv2.rectangle(imgContour, (x , y ), (x + w , y + h ), (0, 255, 0), 5)
-
-                cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,
-                            (0, 255, 0), 2)
-                cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,
-                            (0, 255, 0), 2)
-
 
     print(Red,"   ",Blue,"    ",Black,"     ",Ph,"    "," step output ")
 
@@ -234,9 +195,6 @@ def func_mode2():
     kernel = np.ones((5, 5))
     imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
     getContours(imgDil,imgContour)
-    imgStack = stackImages(0.8,([img,imgCanny],
-                                [imgDil,imgContour]))
-    cv2.imshow("Result", imgStack)
 
 def func_mode1():
 
@@ -257,9 +215,6 @@ def func_mode1():
     mixer.music.play()
     time.sleep(0.5)
 
-    img = cv2.flip(img,1)
-    
-    cv2.imshow('CAMERA',img)
 
 
 while True:
@@ -303,6 +258,9 @@ while True:
             st_time=-1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    img = cv2.flip(img,1)
+    
+    cv2.imshow('CAMERA',img)
 
         
 cap.release()
