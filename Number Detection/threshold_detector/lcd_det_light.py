370a53dc439749c33e0b3358014c1a37360e0a5d
Naman Puri <namanp.ic18@gmail.com>
2020-11-22 18:12:18 +0530

    lcd code up

--- Number Detection/threshold_detector/lcd_det_light.py
+++ Number Detection/threshold_detector/lcd_det_light.py
@@ -6,14 +6,36 @@ import cv2
 
 
 # load the example image
-image = cv2.imread('1.jpeg')
+
+image = cv2.imread('..\\Images_to_test\\9.jpeg')
 # pre-process the image by resizing it, converting it to
 # graycale, blurring it, and computing an edge map
 image = imutils.resize(image, height=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5,5), 0)
-edged = cv2.Canny(blurred, 50, 200, 255) 
+edged = cv2.Canny(blurred, 200, 92, 255)
 
-cv2.imshow("edged",edged)
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+displayCnt = None
+
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	# if the contour has four vertices, then we have found
+	# the display
+	if len(approx) == 4:
+		displayCnt = approx
+		break
+
+warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+output = four_point_transform(image, displayCnt.reshape(4, 2))
+
+cv2.imshow("edged",output)
 
 cv2.waitKey()
